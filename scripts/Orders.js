a5c2d4e8211793af97847d4fe56b172c412f2354
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-07-29 10:02:53 -0500

    edited

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,6 +1,40 @@
-import { getOrders } from "./database.js"
+import { getOrders, getPaints, getInteriors, getTechnologies, getWheels } from "./database.js"
+
+const orders = getOrders()
+const paints = getPaints()
+const interiors = getInteriors()
+const technologies = getTechnologies()
+const wheels = getWheels()
+
+
 
 const buildOrderListItem = (order) => {
+    const findPaint = paints.find(
+        (paint) => {
+            return paint.id === order.paintId
+        }
+    )
+
+    const findInterior = interiors.find(
+        (interior) => {
+            return interior.id === order.interiorId
+        }
+    )
+
+    const findTecnology = technologies.find(
+        (technology) => {
+            return technology.id === order.technologyId
+        }
+    )
+
+    const findWheels = wheels.find (
+        (wheel) => {
+            return wheel.id === order.wheelId
+        }
+    )
+
+        
+
     return `<li>
         Order #${order.id} was placed on ${order.timestamp}
     </li>`
